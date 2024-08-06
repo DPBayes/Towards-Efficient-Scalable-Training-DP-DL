6370eb4aa75eb0edb4dec4c89c6e4adf02bcaa9c
Rodriguez Beltran Sebastian <sebarodr@lx0-500-23094.ad.helsinki.fi>
2024-08-06 17:21:55 +0300

    pytorch pipeline fix

--- torch_exp/thr_record.py
+++ torch_exp/thr_record.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 import torch.multiprocessing as mp
 import socket
+import torch.distributed as dist
 
 
 def get_free_port():
@@ -54,12 +55,12 @@ if __name__ == '__main__':
     t_th = None
     try:
         world_size = torch.cuda.device_count()
-        print(world_size)
-        mp.spawn(main, args=(world_size,port, args), nprocs=world_size)
-        #thr,t_th,accs = main(args)
-        # thr = np.mean(thr)
-        # acc = np.mean(accs)
-        # t_th = np.mean(t_th)
+        dist.init_process_group(backend='nccl')
+        world_size = dist.get_world_size()
+        local_rank = int(os.environ['LOCAL_RANK'])
+        rank = int(os.environ['RANK'])
+        torch.cuda.set_device(local_rank)
+        main(local_rank,rank,world_size,args)
         err = 'None'
     except RuntimeError as e:
         print(e)
