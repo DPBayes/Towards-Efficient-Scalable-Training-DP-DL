1909ee0be04fa69828e580fc98c26ceae742f3ba
Rodriguez Beltran Sebastian <sebarodr@lx0-500-23094.ad.helsinki.fi>
2024-08-20 11:54:49 +0300

    print

--- jax_exp/pipeline_jax.py
+++ jax_exp/pipeline_jax.py
@@ -256,7 +256,7 @@ class TrainerModule:
         predicted_class = jnp.argmax(logits,axis=-1)
         print('logits',logits.shape, flush=True)
         cross_losses = optax.softmax_cross_entropy_with_integer_labels(logits, targets)
-        print('cross_losses:',cross_losses.shape)
+        #print('cross_losses:',cross_losses.shape)
         
 
         cross_loss = jnp.mean(cross_losses)
