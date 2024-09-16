e91ffc97cc713ebe1d979aa1a02e191987b7886c
Rodriguez Beltran Sebastian <sebarodr@lx0-500-23094.ad.helsinki.fi>
2024-09-16 15:16:36 +0300

    check input type

--- jax_exp/jax_from_0.py
+++ jax_exp/jax_from_0.py
@@ -234,7 +234,7 @@ def eval_step_non(model,params, batch):
 def train_step(state,batch):
     def loss_fn(params):
         inputs,targets = batch
-        logits = state.apply_fn(params,inputs)
+        logits, _  = state.apply_fn(params,inputs)
         predicted_class = jnp.argmax(logits,axis=-1)
 
         cross_loss = optax.softmax_cross_entropy_with_integer_labels(logits, targets).mean()
@@ -267,7 +267,8 @@ def train_epoch(state,data_loader):
 #@jit
 def eval_step(state,batch):
     inputs,targets = batch
-    logits = state.apply_fn(state.params,inputs)
+    print(type(inputs))
+    logits, _ = state.apply_fn(state.params,inputs)
     predicted_class = jnp.argmax(logits,axis=-1)
     #cross_losses = optax.softmax_cross_entropy_with_integer_labels(logits, targets)
     #cross_loss = jnp.mean(cross_losses)
