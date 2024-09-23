d75a29c72f812b8019a96168d2d2944d9651802c
Rodriguez Beltran Sebastian <sebarodr@lx0-500-23094.ad.helsinki.fi>
2024-09-23 16:08:14 +0300

    add comment to check the types

--- jax_exp/pipeline_jax_new.py
+++ jax_exp/pipeline_jax_new.py
@@ -592,8 +592,9 @@ class TrainerModule:
             print('physical bs',len(pb),'physical bs labels',len(yb),'mask',len(mask))
             print('mask \n',mask)
             sum_of_clipped_grads_from_pb,loss_sum,mean_acc,sum_corr = self.process_a_physical_batch_non(params,(pb,yb),mask)
-            print(sum_of_clipped_grads_from_pb)
-            print(accumulated_grads)
+            print('sum',sum_of_clipped_grads_from_pb)
+            print('\n ---------------------------- \n')
+            print('acc',accumulated_grads)
             accumulated_grads = jax.tree_map(lambda x,y: x+y, 
                                                 accumulated_grads, 
                                                 sum_of_clipped_grads_from_pb
