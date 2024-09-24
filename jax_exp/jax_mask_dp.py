16c3e3e7dc6ea3da11dec91ad6aac153de6c98fd
Rodriguez Beltran Sebastian <sebarodr@lx0-500-23094.ad.helsinki.fi>
2024-09-24 19:14:35 +0300

    fix input dict

--- jax_exp/jax_mask_dp.py
+++ jax_exp/jax_mask_dp.py
@@ -430,7 +430,7 @@ def main(args):
         print('after',e,'epoch','iteration',t)
         #eval(state,)
 
-main({'dimension':224,'epochs':2,'clipping_mode':'private','num_classes':100,'model_name':'google/vit-base-patch16-224','lr':0.00031,'bs':25000,'pbs':50})
+main(dict({'dimension':224,'epochs':2,'clipping_mode':'private','num_classes':100,'model_name':'google/vit-base-patch16-224','lr':0.00031,'bs':25000,'pbs':50}))
 
     # _, barely_clipped_grad, actual_batch_size = private_iteration_v2((batch_X, batch_y), state, k, q, t, 0.0, 1000.0, n)
     # _, just_clipped_grad, actual_batch_size = private_iteration_v2((batch_X, batch_y), state, k, q, t, 0.0, 1.0, n)
