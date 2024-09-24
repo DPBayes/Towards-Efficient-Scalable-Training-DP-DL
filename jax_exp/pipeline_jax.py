6f137265c8b7adb264becf448d3cda36627fb2bf
Rodriguez Beltran Sebastian <sebarodr@lx0-500-23094.ad.helsinki.fi>
2024-09-24 10:18:00 +0300

    only head test

--- jax_exp/pipeline_jax.py
+++ jax_exp/pipeline_jax.py
@@ -1360,8 +1360,8 @@ class TrainerModule:
             variables = model.init({'params':init_rng},x)
 
             #So far, the parameters are initialized randomly, so we need to unfreeze them and add the pre loaded parameters.
-            params = freeze(variables['params'])
-            params['vit'] = vars['params']
+            params = variables['params']
+            params['vit'] = freeze(vars['params'])
             #params = unfreeze(params)
             #self.print_param_shapes(params)
             #print(params)
