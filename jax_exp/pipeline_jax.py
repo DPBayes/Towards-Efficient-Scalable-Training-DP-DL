954e70a8dc06bf2d4332d19540b6d886e083d9c6
Rodriguez Beltran Sebastian <sebarodr@lx0-500-23094.ad.helsinki.fi>
2024-09-25 11:05:21 +0300

    add the normalization after the noise for dp

--- jax_exp/pipeline_jax.py
+++ jax_exp/pipeline_jax.py
@@ -607,10 +607,12 @@ class TrainerModule:
                     accumulated_iterations += 1
                     if not flag._check_skip_next_step():
                         print('about to update:')
-                        acc_grads = jax.tree_util.tree_map(
-                            lambda x: x/expected_bs*accumulated_iterations,
-                            acc_grads)
                         updates,self.rng = add_noise_fn(self.noise_multiplier*self.max_grad_norm,self.rng,acc_grads)
+
+
+                        updates = jax.tree_util.tree_map(
+                            lambda x: x/expected_bs*accumulated_iterations,
+                            updates)
 
                         #old_params = self.params
                         #self.params,self.opt_state = jax.block_until_ready(self.grad_acc_update(acc_grads,self.opt_state,self.params))
