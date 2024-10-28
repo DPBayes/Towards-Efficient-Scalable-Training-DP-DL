fix: fix the accuracy calculation, and fix some bugs in parameters names

src/jax_mask_efficient.py
@@ -6,6 +6,7 @@ from flax.training import train_state
 
 from data import normalize_and_reshape
 
+import warnings
 
 ## define some jax utility functions
 
@@ -297,13 +298,21 @@ def compute_accuracy_for_batch(
 
 
 def model_evaluation(
-    state: train_state.TrainState, test_data: jax.typing.ArrayLike, test_labels: jax.typing.ArrayLike
+    state: train_state.TrainState, test_images: jax.typing.ArrayLike, test_labels: jax.typing.ArrayLike, batch_size:int = 50
 ):
+    diff = len(test_images) % batch_size
 
+    if diff != 0:
+        batch_size = batch_size - diff
+        warnings.warn(f'The batch size does not divide the size of the test set, fixed the new batch size to {batch_size}')
+
+    num_splits = int(len(test_images)/batch_size)
+    splits_test = jnp.split(test_images, num_splits)
+    splits_test_labels = jnp.split(test_labels, num_splits)
     corr = 0 
     total = 0
 
-    for pb, yb in zip(test_data, test_labels):
+    for pb, yb in zip(splits_test, splits_test_labels):
         pb = jax.device_put(pb, jax.devices("gpu")[0])
         yb = jax.device_put(yb, jax.devices("gpu")[0])
         # TODO: This won't be correct when len(pb) not the same for all pb in test_data.