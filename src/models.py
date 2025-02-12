a1e9522cea7c01d0b79ce37f06c11c69c22efcb7
Solosneros <24623119+Solosneros@users.noreply.github.com>
2025-02-12 15:15:37 +0200

    fix: support flax and non flax and freeze layers in a better way

--- src/models.py
+++ src/models.py
@@ -12,7 +12,7 @@ def create_train_state(
     model_name: str, num_classes: int, image_dimension: int, optimizer_config: namedtuple, layers_to_freeze=None
 ):
     """Creates initial `TrainState`."""
-    rng, model, params = load_model(jax.random.key(0), model_name, image_dimension, num_classes)
+    rng, model, params, from_flax = load_model(jax.random.key(0), model_name, image_dimension, num_classes)
 
     if layers_to_freeze is None:
         tx = optax.adam(optimizer_config.learning_rate)
@@ -20,17 +20,26 @@ def create_train_state(
         # https://flax.readthedocs.io/en/v0.6.11/guides/transfer_learning.html
         params = freeze(params)
         partition_optimizers = {"trainable": optax.adam(optimizer_config.learning_rate), "frozen": optax.set_to_zero()}
+        
+        # the paths are joined by "." here so that we can just check if the layer_to_freeze is in the path (substring match)
         param_partitions = freeze(
             traverse_util.path_aware_map(
                 lambda path, v: (
-                    "frozen" if any([layer_to_freeze in path for layer_to_freeze in layers_to_freeze]) else "trainable"
+                    "frozen"
+                    if any([layer_to_freeze in ".".join(path) for layer_to_freeze in layers_to_freeze])
+                    else "trainable"
                 ),
                 params,
             )
         )
         tx = optax.multi_transform(partition_optimizers, param_partitions)
 
-    return train_state.TrainState.create(apply_fn=jax.jit(model.__call__), params=params, tx=tx)
+    if from_flax:
+        return train_state.TrainState.create(apply_fn=model.__call__, params=params, tx=tx)
+    else:
+        return train_state.TrainState.create(
+            apply_fn=lambda x, params: model.apply({"params": params}, x), params=params, tx=tx
+        )
 
 
 def load_model(rng, model_name, dimension, num_classes):
@@ -49,7 +58,7 @@ def load_model(rng, model_name, dimension, num_classes):
                 x = x.reshape((x.shape[0], -1))
                 x = nn.Dense(features=256)(x)
                 x = nn.relu(x)
-                x = nn.Dense(features=100)(x)
+                x = nn.Dense(features=num_classes)(x)
                 return x
 
         model = CNN()
@@ -65,7 +74,7 @@ def load_model(rng, model_name, dimension, num_classes):
         variables = model.init({"params": init_rng}, x)
         # variables = model.init({'params':main_key}, batch)
         model.apply(variables, x)
-        return main_rng, model, variables["params"]
+        return main_rng, model, variables["params"], False
 
     elif "vit" in model_name:
         model = FlaxViTForImageClassification.from_pretrained(
@@ -74,4 +83,4 @@ def load_model(rng, model_name, dimension, num_classes):
             return_dict=False,
             ignore_mismatched_sizes=True,
         )
-        return main_key, model, model.params
+        return main_key, model, model.params, True
